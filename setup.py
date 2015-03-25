44cba1160a9d218553d0cf63c40b3d90186d128e
Keyan Pishdadian <kpishdadian@gmail.com>
2015-03-25 00:04:39 -0400

    Initial commit
    Transfers script from Flask and starts building up the package contents.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='flask_ext_migrate',
+    version='0.0.1',
+    url='https://github.com/pocoo/flask-ext-migrate',
+    license='BSD',
+    author='Keyan Pishdadian',
+    author_email='kpishdadian@gmail.com',
+    description='A sourcecode manipulation tool for converting imports.',
+    long_description='This tool allows for rapid migration of extension '
+                     'imports away from the deprecated `.ext` format.',
+    install_requires=['RedBaron'],
+    packages=['flask_ext_migrate'],
+    entry_points={
+        'console_scripts': ['flask_ext_migrate = pour.pour:generate']
+    }
+
+)
