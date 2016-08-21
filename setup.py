a505999d5cdd8e918a84952d7f9790c5aec0cfed
Keyan Pishdadian <kpishdadian@gmail.com>
2016-08-21 13:11:27 -0400

    Bump version, fixes #13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='flask_ext_migrate',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/pocoo/flask-ext-migrate',
     license='BSD',
     author='Keyan Pishdadian',
