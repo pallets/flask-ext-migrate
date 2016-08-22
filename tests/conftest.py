ab299d7fd2aa9645f53afe9671ec87222c01bfc7
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-08-22 03:37:47 +0200

    Use pytest's tmpdir fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,3 @@ import pytest
 @pytest.fixture(scope='function')
 def runner(request):
     return CliRunner()
-
-
-@pytest.fixture(scope='function')
-def temp_dir():
-    temp_directory = tempfile.mkdtemp()
-    yield temp_directory
-    # Teardown
-    shutil.rmtree(temp_directory)
--- tests/test_script.py
+++ tests/test_script.py
@@ -4,28 +4,26 @@ from flask_ext_migrate.startup import startup
 import flask_ext_migrate as migrate
 
 
-def test_single_file_input_runs_without_failures(runner, temp_dir):
+def test_single_file_input_runs_without_failures(runner, tmpdir):
     import_line = 'from flask.ext.foo import bar'
-    temp_file = os.path.join(temp_dir, 'temp.py')
+    temp_file = tmpdir.join('temp.py')
+    temp_file.write(import_line)
 
-    with open(temp_file, 'w') as f:
-        f.write(import_line)
-
-    result = runner.invoke(startup, [temp_file])
+    result = runner.invoke(startup, [str(temp_file)])
     assert result.exit_code == 0
+    assert temp_file.read() == migrate.fix_tester(import_line)
 
 
-def test_no_file_arg_fails(runner, temp_dir):
+def test_no_file_arg_fails(runner):
     result = runner.invoke(startup, [])
     assert result.exit_code != 0
 
 
-def test_single_file_run_modifies_file_properly(runner, temp_dir):
+def test_single_file_run_modifies_file_properly(runner, tmpdir):
     import_line = 'from flask.ext.foo import bar'
-    temp_file = os.path.join(temp_dir, 'temp.py')
+    temp_file = tmpdir.join('temp.py')
+    temp_file.write(import_line)
 
-    with open(temp_file, 'w') as f:
-        f.write(import_line)
-    runner.invoke(startup, [temp_file])
-
-    assert open(temp_file, 'r').read() == migrate.fix_tester(import_line)
+    result = runner.invoke(startup, [str(temp_file)])
+    assert result.exit_code == 0
+    assert temp_file.read() == migrate.fix_tester(import_line)
